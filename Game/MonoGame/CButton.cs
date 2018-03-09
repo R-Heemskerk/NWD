db2b852f73029c87f719da7a50389ef01b2e2e4c
NialvdSteeg <35953794+NialvdSteeg@users.noreply.github.com>
2018-03-09 09:21:29 +0100

    Refresh part 2 - the beginning

---
+++ Game/MonoGame/CButton.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame
+{
+    class CButton
+    {
+        Texture2D texture;
+        Vector2 position;
+        Rectangle rectangle;
+
+        // Changes colour if mouse moves over it
+        Color colour = new Color(255, 255, 255, 255);
+
+        public Vector2 size;
+
+        public CButton(Texture2D newTexture, GraphicsDevice graphics)
+        {
+            texture = newTexture;
+
+            // ScreenWidth = 800 , ScreenHeight = 600
+            // ImageWidth = 100, ImageHeight = 20
+            // Keeps the right proportions between image and screenwith
+
+            size = new Vector2(graphics.Viewport.Width / 8, graphics.Viewport.Height / 30);
+
+        }
+
+        bool down;
+        public bool isClicked;
+        public void Update(MouseState mouse)
+        {
+            //Places the button on the screen
+            rectangle = new Rectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y);
+
+            //Makes a small rectangle at position of the mouse
+            Rectangle mouseRectangle = new Rectangle(mouse.X, mouse.Y, 1, 1);
+
+            //If the two rectangles collide th button will light up
+            if (mouseRectangle.Intersects(rectangle))
+            {
+                //If the colour is equal to the colour in line 19 than the mouse is not intersect the rectangle
+                if (colour.A == 255) down = false;
+                if (colour.A == 0) down = true;
+                if (down) colour.A += 3; else colour.A -= 3;
+                if (mouse.LeftButton == ButtonState.Pressed) isClicked = true;
+            }
+
+            else if (colour.A < 255)
+            {
+                colour.A += 3;
+                isClicked = false;
+            }
+        }
+        public void setPosition(Vector2 newPosition)
+        {
+            position = newPosition;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, rectangle, colour);
+        }
+    }
+}
---
+++ Game/MonoGame/Game1.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGame
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Dice dieSet = new Dice();
+        KeyboardState prevStatekeyboard, curStatekeyboard;
+
+        enum GameState 
+        {
+            MainMenu,
+            Options,
+            Playing,
+        }
+        // Makes the current gamestate go to the menu
+        GameState CurrentGameState = GameState.MainMenu;
+
+        //Screensize adjustments (this is the standard resolution of my screen)
+        int screenWidth = 1366, screenHeight = 768;
+
+        CButton btnPlay;
+        
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            curStatekeyboard = prevStatekeyboard = Keyboard.GetState();
+            
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //screen stuff
+            graphics.PreferredBackBufferWidth = screenWidth;
+            graphics.PreferredBackBufferHeight = screenHeight;
+            graphics.ApplyChanges();
+            IsMouseVisible = true;
+
+            btnPlay = new CButton(Content.Load<Texture2D>("Button"), graphics.GraphicsDevice);
+            btnPlay.setPosition(new Vector2(650, 375));
+
+            dieSet.LoadContent(Content);
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            MouseState mouse = Mouse.GetState();
+            curStatekeyboard = Keyboard.GetState();
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            switch (CurrentGameState)
+            {
+                case GameState.MainMenu:
+                    if (btnPlay.isClicked == true) CurrentGameState = GameState.Playing;
+                    btnPlay.Update(mouse);
+                    break;
+
+                case GameState.Playing:
+                    break;
+            }
+
+
+            // If space bar is pressed in the current state and the previous state the space bar was not pressed then the dice are rolled.
+            if (curStatekeyboard.IsKeyDown(Keys.Space) && prevStatekeyboard.IsKeyUp(Keys.Space))
+                dieSet.Roll();
+            // TODO: Add your update logic here
+
+
+            prevStatekeyboard = curStatekeyboard;
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            switch (CurrentGameState)
+            {
+                case GameState.MainMenu:
+                    btnPlay.Draw(spriteBatch);
+                    break;
+
+                case GameState.Playing:
+                    break;
+            }
+            dieSet.Draw(spriteBatch);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
